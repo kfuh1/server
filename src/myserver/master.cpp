ea5b1c300c092b3bc69fc1f8d54d700fb3e98abd
Kathleen Fuh <kfuh@latedays.andrew.cmu.edu>
2016-03-25 14:43:30 -0400

    remove linear scaling for countprimes

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -25,7 +25,7 @@ struct Worker_state {
   int num_cpu_intense_requests;
   int num_non_intense_requests; //this would be like tellmenow
 
-  int weighted_countprimes_requests;
+//  int weighted_countprimes_requests;
 
   int num_cache_intense_requests;
   int num_pending_requests; //this is the total number of pending reqs
@@ -63,7 +63,7 @@ std::unordered_map<int, std::string> tagToReqMap;
 std::unordered_map<int, std::string> tagToTypeMap; //the string is type we define
 std::unordered_map<int, int> cmpPrimeTagToTagMap;
 std::unordered_map<int, cmp_primes_data> tagToCmpPrimesDataMap;
-std::unordered_map<int, int> tagToPrimeMap;
+//std::unordered_map<int, int> tagToPrimeMap;
 
 static struct Request_cache {
   int size;
@@ -100,7 +100,7 @@ void master_node_init(int max_workers, int& tick_period) {
     ws.num_cache_intense_requests = 0;
     ws.num_cpu_intense_requests = 0;
     ws.num_non_intense_requests = 0;
-    ws.weighted_countprimes_requests = 0;
+//    ws.weighted_countprimes_requests = 0;
     ws.num_pending_requests = 0;
     ws.to_be_killed = false;
     ws.is_alive = false;
@@ -128,7 +128,7 @@ void handle_new_worker_online(Worker_handle worker_handle, int tag) {
   mstate.worker_states[idx].num_cache_intense_requests = 0;
   mstate.worker_states[idx].num_cpu_intense_requests = 0;
   mstate.worker_states[idx].num_non_intense_requests = 0;
-  mstate.worker_states[idx].weighted_countprimes_requests = 0;
+//  mstate.worker_states[idx].weighted_countprimes_requests = 0;
   mstate.worker_states[idx].num_pending_requests = 0;
   mstate.worker_states[idx].to_be_killed = false;
   
@@ -225,20 +225,20 @@ void handle_worker_response(Worker_handle worker_handle, const Response_msg& res
       tagToTypeMap.erase(tag);
 
       //decrement the weighted count for countprimes requests
-      if(tagToPrimeMap.find(tag) != tagToPrimeMap.end()){
-        int cat = tagToPrimeMap.at(tag);
-        ws.weighted_countprimes_requests -= cat;
-        tagToPrimeMap.erase(tag);
-      }
+//      if(tagToPrimeMap.find(tag) != tagToPrimeMap.end()){
+//        int cat = tagToPrimeMap.at(tag);
+//        ws.weighted_countprimes_requests -= cat;
+//        tagToPrimeMap.erase(tag);
+//      }
 
       //summing up manually because ws.num_pending_requests is wrong
       int totalRequests = ws.num_cache_intense_requests + ws.num_cpu_intense_requests + ws.num_non_intense_requests;
       //kill the worker if it's been flagged and it's done with work
       if(ws.to_be_killed && totalRequests == 0){
-
+/*
         std::cout << "\n---------------KILLING A WORKER-----------------------\n";
         std::cout << "Current tag and timestamp " << mstate.next_tag << "\n";
-
+*/
 /*
         t = CycleTimer::currentSeconds(); 
         std::cout << "\n---------------KILLING ALL THE WORKERS-----------------------\n";
@@ -286,7 +286,7 @@ if(mstate.worker_states[i].is_alive){
   }
 
 }
-
+/*
 int find_min_primes_idx(){ 
   int curr_min;
   int selected_idx;
@@ -310,7 +310,7 @@ int find_min_primes_idx(){
   }
   return selected_idx;
 }
-
+*/
 int find_min_cache_idx(){
   int curr_min;
   int selected_idx;
@@ -416,12 +416,12 @@ int choose_worker_idx(int tag){
     return find_min_cache_idx();
   }
   else if(type == "cpu"){
-    if(tagToPrimeMap.find(tag) != tagToPrimeMap.end()){
-      return find_min_primes_idx();
-    }
-    else{
+//    if(tagToPrimeMap.find(tag) != tagToPrimeMap.end()){
+//      return find_min_primes_idx();
+//    }
+//    else{
       return find_min_cpu_idx();
-    }
+//    }
   }
   else if(type == "non"){
     return find_min_non_idx();
@@ -449,7 +449,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   bool not_intense = false;
 
 
-  int weighted_countprimes = 0;
+//  int weighted_countprimes = 0;
 
   DLOG(INFO) << "Received request: " << client_req.get_request_string() << std::endl;
 
@@ -522,11 +522,11 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   }
   //countprimes 418wisdom - debating whether or not we want catch-all case 
   else{
-    if(req_name == "countprimes"){
-      int n = atoi(client_req.get_arg("n").c_str());
-      tagToPrimeMap.insert(std::pair<int,int>(tag, n));
-      weighted_countprimes = n;
-    }
+//    if(req_name == "countprimes"){
+//      int n = atoi(client_req.get_arg("n").c_str());
+//      tagToPrimeMap.insert(std::pair<int,int>(tag, n));
+//      weighted_countprimes = n;
+  //  }
     tagToTypeMap.insert(std::pair<int,std::string>(tag, "cpu"));
     is_cpu_intense = true;
   }
@@ -544,7 +544,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
   else if(not_intense){
     mstate.worker_states[idx].num_non_intense_requests++;
   }
-  mstate.worker_states[idx].weighted_countprimes_requests += weighted_countprimes;
+//  mstate.worker_states[idx].weighted_countprimes_requests += weighted_countprimes;
   mstate.worker_states[idx].num_pending_requests++;
   mstate.next_tag++;
   send_request_to_worker(ws.worker_handle, worker_req);
@@ -552,7 +552,7 @@ void handle_client_request(Client_handle client_handle, const Request_msg& clien
 
 
 void handle_tick() {
-
+/*
         t = CycleTimer::currentSeconds(); 
         std::cout << "\n--------------HANDLE TICK-----------------------\n";
         std::cout << "num_pending_client_requests: " << mstate.num_pending_client_requests << "\n";
@@ -568,7 +568,7 @@ void handle_tick() {
         }
 
         std::cout << "\n------------------------------------------------------\n\n";
-
+*/
   int num_cpu = 0;
   int num_cache = 0;
 
@@ -591,10 +591,10 @@ void handle_tick() {
     int avg_work_per_node = weighted_total / num_actually_alive;
     
     if(avg_cpu_intense_work > 3 * MAX_THREADS / 4  || avg_cache_intense_work > 1){
- 
+ /*
         std::cout << "\n---------------ADDING A NEW WORKER-----------------------\n";
         std::cout << "Current tag and timestamp " << mstate.next_tag << "\n";
-
+*/
     /* 
         t = CycleTimer::currentSeconds(); 
         std::cout << "\n---------------ADDING A NEW WORKER-----------------------\n";
@@ -649,10 +649,10 @@ void handle_tick() {
     int avg_cpu_intense_work = num_cpu / (num_actually_alive - 1);
     int avg_cache_intense_work = num_cache / (num_actually_alive - 1);
     if(avg_cpu_intense_work < MAX_THREADS - 2 && avg_cache_intense_work < 1){
-      
+  /*    
       std::cout << "\n---------------SETTING TO BE KILLED FLAG-----------------------\n";
       std::cout << "Current tag and timestamp " << mstate.next_tag << "\n";
-
+*/
      /*
       t = CycleTimer::currentSeconds(); 
       std::cout << "\n---------------SETTING TO BE KILLED FLAG-----------------------\n";
