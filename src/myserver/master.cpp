7f028f412c1d166a8212f4095c21c22455c073a4
Kathleen Fuh <kfuh@latedays.andrew.cmu.edu>
2016-03-25 05:45:29 -0400

    Change threshold to add worker to 3/4 MAX_THREADS

--- src/myserver/master.cpp
+++ src/myserver/master.cpp
@@ -539,7 +539,7 @@ void handle_tick() {
 
     int avg_work_per_node = weighted_total / num_actually_alive;
     
-    if(avg_cpu_intense_work > MAX_THREADS - 2 || avg_cache_intense_work > 1){
+    if(avg_cpu_intense_work > 3 * MAX_THREADS / 4 || avg_cache_intense_work > 1){
  
         std::cout << "\n---------------ADDING A NEW WORKER-----------------------\n";
         std::cout << "Current tag and timestamp " << mstate.next_tag << "\n";
